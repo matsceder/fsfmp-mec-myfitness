2c9b6eb9fb6366e26c70a92127c8d5711c88696b
matsceder <mats_ceder@hotmail.com>
2020-07-21 10:07:28 +0000

    customized products admin

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -33,7 +37,6 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    admin_friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.admin_friendly_name
+        return self.product
