947eb026d6c767609d1c5c5407d972fa8f1ecd16
matsceder <mats_ceder@hotmail.com>
2020-08-07 10:32:48 +0000

    putting in the last details to toasts

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -13,16 +13,17 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ A view to add products in the shopping cart """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.brand} - {product} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
-        messages.success(request, f'Added {product.brand} - {product} to your cart')
+        messages.success(request, f'Added {cart[item_id]} * {product.brand} - {product} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -31,13 +32,17 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """ Adjust the quantity of products in the shopping cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated {product.brand} - {product} quantity to {cart[item_id]}')
+
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.brand} - {product} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('cart'))
@@ -47,12 +52,15 @@ def remove_from_cart(request, item_id):
     """ Remove and item from the shopping cart """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.brand} - {product} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item {e}')
         return HttpResponse(status=500)
