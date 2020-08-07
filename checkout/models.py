43d91499500ac85df121a5b18a0541f23fdf2e1b
matsceder <mats_ceder@hotmail.com>
2020-08-07 16:44:48 +0000

    created checkout admin, signals and forms

--- checkout/models.py
+++ checkout/models.py
@@ -28,7 +28,10 @@ class Order(models.Model):
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
-        """ Update grand total when a new line item is added, also accounting delivery costs """
+        """
+        Update grand total when a new line item is added,
+        also accounting delivery costs
+        """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -38,7 +41,10 @@ class Order(models.Model):
         self.save()
 
     def save(self, *args, **kwargs):
-        """ Overriding save method to set order number if that has not already been set """
+        """
+        Overriding save method to set order
+        number if that has not already been set
+        """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
@@ -54,7 +60,10 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
-        """ Overriding save method to set the lineitem total and update grand total """
+        """
+        Overriding save method to set the
+        lineitem total and update grand total
+        """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
