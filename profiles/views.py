d3036bf59433fd3f9a96c78274254d1ec6c331b2
matsceder <mats_ceder@hotmail.com>
2020-08-12 11:18:04 +0000

    added profiles app and set up template basics

---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def profile(request):
+    """ A view to display the user profiles """
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
