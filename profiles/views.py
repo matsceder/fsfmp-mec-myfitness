cc79e015dfe2db6106d723b46d5d436261d85784
matsceder <mats_ceder@hotmail.com>
2020-08-13 11:02:37 +0000

    working on profiles. can't get saved info to display in profile template

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 def profile(request):
@@ -23,6 +24,23 @@ def profile(request):
         'orders': orders,
         'form': form,
         'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a confirmation for order {order_number} placed on {order.date}.\
+        A confirmation email was sent on the order date'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
