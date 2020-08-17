ae7193aeba8ffe28acdedab1cb7bf47c2cded2ef
matsceder <mats_ceder@hotmail.com>
2020-08-17 22:06:47 +0000

    programs - added template and tested adding programs just to make sure they render as intended.

--- programs/views.py
+++ programs/views.py
@@ -4,9 +4,11 @@ from .models import Programs
 
 def programs(request):
     """ A view to display the different workout and diet-programs """
+    programs = Programs.objects.all()
+
     template = 'programs/programs.html'
     context = {
-
+        'programs': programs,
     }
 
     return render(request, template, context)
