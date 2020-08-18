017c7401920c867a4a3a135b9756bbbbeac61b2d
matsceder <mats_ceder@hotmail.com>
2020-08-18 07:41:44 +0000

    programs - added delete post url, and view

--- programs/urls.py
+++ programs/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.programs, name='programs'),
     path('program/<int:program_id>/', views.program, name='program'),
     path('add_post/', views.add_program, name='add_program'),
+    path('edit_post/<int:program_id>/', views.edit_program, name='edit_program'),
+    path('delete_post/<int:program_id>/', views.delete_program, name='delete_program'),
 ]
--- programs/views.py
+++ programs/views.py
@@ -55,3 +55,45 @@ def add_program(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_program(request, program_id):
+    """ Adding new Program post """
+    if not request.user.is_superuser:
+        messages.error(request, "You're not authorized to do this")
+        return redirect(reverse('home'))
+
+    program = get_object_or_404(Programs, pk=program_id)
+    if request.method == 'POST':
+        programs_form = ProgramsForm(request.POST, request.FILES, instance=program)
+        if programs_form.is_valid():
+            programs_form.save()
+            messages.success(request, "Post successfully updated")
+            return redirect(reverse('program', args=[program.id]))
+        else:
+            messages.error(request, "Failed to save post. Make sure form is valid")
+    else:
+        programs_form = ProgramsForm(instance=program)
+        messages.info(request, f"You're now making changes to {program.title} by {program.author}")
+
+    template = 'programs/edit_program.html'
+    context = {
+        'program': program,
+        'programs_form': programs_form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_program(request, program_id):
+    """ Deletes a program post """
+    if not request.user.is_superuser:
+        messages.error(request, "You're not authorized to do this")
+        return redirect(reverse('home'))
+
+    program = get_object_or_404(Programs, pk=program_id)
+    program.delete()
+    messages.success(request, 'Program post deleted!')
+    return redirect(reverse('programs'))
